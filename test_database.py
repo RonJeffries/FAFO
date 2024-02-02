1a9e681bd94587b7d6e24a194cb8ba15786d8225
Ron Jeffries <ronjeffries@acm.org>
2024-02-02 10:04:40 -0500

    changed file name format

--- test_database.py
+++ test_database.py
@@ -15,9 +15,9 @@ class TestDatabase:
 
     def test_find_hill_math(self):
         all_names = self.get_all_filenames()
-        hill = [name for name in all_names if "author-geepaw" in name]
-        math_hill = [name for name in hill if "topic-math" in name]
-        hector = [name for name in math_hill if "student-hector" in name]
+        hill = [name for name in all_names if "_author-geepaw_" in name]
+        math_hill = [name for name in hill if "_topic-math_" in name]
+        hector = [name for name in math_hill if "_student-hector_" in name]
         assert len(hill) == 100
         assert len(math_hill) == 10
         assert len(hector) == 1
