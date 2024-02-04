cff2b30a264aceee4dee3a55539b198c94e20f5f
Ron Jeffries <ronjeffries@acm.org>
2024-02-04 09:19:26 -0500

    initial definition of Atom with tests.

--- test_xst.py
+++ test_xst.py
@@ -1,5 +1,15 @@
-
+from collections import namedtuple
+Atom = namedtuple("Atom", ["element", "scope"])
 
 class TestXST:
-    def test_hookup(self):
-        assert 2 + 2 == 4
+    def test_tuples(self):
+        a1 = Atom(1, 2)
+        a2 = Atom(2-1, 3-1)
+        a3 = Atom(2, 1)
+        assert a1 == a2
+        assert not a1 == a3
+
+    def test_members(self):
+        a1 = Atom (31, 42)
+        assert a1.element == 31
+        assert a1.scope == 42
