f57b042dff5321ce8e483704d552c65377411a60
Ron Jeffries <ronjeffries@acm.org>
2024-02-04 09:25:40 -0500

    testing atom in set.

--- test_xst.py
+++ test_xst.py
@@ -13,3 +13,12 @@ class TestXST:
         a1 = Atom (31, 42)
         assert a1.element == 31
         assert a1.scope == 42
+
+    def test_set_in(self):
+        a1 = Atom("jeffries", "name")
+        a2 = Atom("hendrickson", "name")
+        a3 = Atom("hill", "name")
+        s1 = {a1, a2}
+        assert a1 in s1
+        assert a2 in s1
+        assert a3 not in s1
