93141af9fb04e75f5f7acd0d13140145e343bf81
Ron Jeffries <ronjeffries@acm.org>
2024-03-02 15:46:32 -0500

    working on dijkstra parsing

--- tst/test_functions.py
+++ tst/test_functions.py
@@ -1,4 +1,30 @@
+import pytest
+
 from xset import XSet
+import re
+
+
+def dij(lexed: list):
+    priority = { '+':1, '-':1, '*': 2}
+    stack = []
+    output = []
+    expr = lexed[::-1]
+    while expr:
+        token = expr.pop()
+        if token[0].isalpha():
+            output.append(token)
+        elif token[0].isnumeric():
+            output.append(token)
+        else:
+            o1 = token
+            p1 = priority[o1]
+            while stack and stack[-1] != '(' and priority[stack[-1]] > p1:
+                o2 = stack.pop()
+                output.append(o2)
+            stack.append(o1)
+    while stack:
+        output.append(stack.pop())
+    return output
 
 
 class TestFunctions:
@@ -19,5 +45,27 @@ class TestFunctions:
         new_set = XSet.n_tuple(result)
         c1 = new_set[1]
         assert c1['c'] == 3
+
+    def test_lexing(self):
+        expr = 'a123 + 37*b17 - c*37.5'
+        no_space = expr.replace(' ', '')
+        rx = '([^a-zA-Z0-9.])'
+        lexed = re.split(rx, no_space)
+        expected = ['a123', '+', '37', '*', 'b17', '-', 'c', '*', '37.5']
+        assert lexed == expected
+
+    def test_dijkstra_1(self):
+        lexed = ['a123', '+', '37']
+        rpn = dij(lexed)
+        assert rpn == ['a123', '37', '+']
+
+    def test_dijkstra_2(self):
+        lexed = ['a123', '+', '37', '*', 'b17', '-', 'c', '*', '37.5']
+        rpn = dij(lexed)
+        assert rpn == ['a123', '37', 'b17', '*', 'c', '37.5', '*', '-', '+']
+
+    def test_int(self):
+        with pytest.raises(ValueError):
+            int('1.1')
 
 
