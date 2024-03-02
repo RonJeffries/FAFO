d87448c65437446beda3ac2c8b0c8d2e4a0629aa
Ron Jeffries <ronjeffries@acm.org>
2024-03-02 08:49:12 -0500

    SetBuilder creates Xset.

--- tst/test_set_builder.py
+++ tst/test_set_builder.py
@@ -1,7 +1,25 @@
+from xset import XSet
+
+
 class SetBuilder:
-    pass
+    def __init__(self):
+        self.contents = []
+
+    def put(self, element, scope):
+        self.contents.append((element, scope))
+
+    def set(self):
+        return XSet.from_tuples(self.contents)
 
 
 class TestSetBuilder:
     def test_exists(self):
         sb = SetBuilder()
+
+    def test_small_set(self):
+        sb = SetBuilder()
+        sb.put('a', 'A')
+        sb.put('b', 'B')
+        xset = sb.set()
+        assert xset['A'] == 'a'
+        assert xset['B'] == 'b'
