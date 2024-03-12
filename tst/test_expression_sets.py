d19a6e32d6bfba698bdef3826b8bb3376d4c096a
Ron Jeffries <ronjeffries@acm.org>
2024-03-12 09:34:57 -0400

    additional test.

--- tst/test_expression_sets.py
+++ tst/test_expression_sets.py
@@ -61,3 +61,20 @@ class TestExpressionSets:
                 assert e['name'] == 'sam'
                 assert e['total_pay'] == '54321'
                 assert e['double'] == '8040'
+
+    def test_x_calculation_patch_set(self):
+        p1 = XSet.from_tuples((('joe', 'name'),('10000', 'salary'), ('2345', 'bonus')))
+        p2 = XSet.from_tuples((('sam', 'name'),('50301', 'salary'), ('4020', 'bonus')))
+        personnel = XSet.n_tuple((p1, p2))
+        total = 'total_pay = salary + bonus'
+        double = 'double = bonus * 2'
+        x_calc = XCalculation(personnel, [total, double])
+        calc_set = XSet(x_calc)
+        joe_rec = calc_set[1]
+        assert joe_rec['name'] == 'joe'
+        assert joe_rec['total_pay'] == '12345'
+        assert joe_rec['double'] == '4690'
+        sam_rec = calc_set[2]
+        assert sam_rec['name'] == 'sam'
+        assert sam_rec['total_pay'] == '54321'
+        assert sam_rec['double'] == '8040'
