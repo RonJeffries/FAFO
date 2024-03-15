a06d72053c60e08844fad1235835c38d61e263e7
Ron Jeffries <ronjeffries@acm.org>
2024-03-15 13:05:16 -0400

    initial XGroup produces keys component.

--- tst/test_group.py
+++ tst/test_group.py
@@ -1,16 +1,27 @@
 from collections import defaultdict
 
 from set_builder import SetBuilder
+from ximpl import XImplementation
 from xset import XSet
 
 
-def find(group_dictionary, department, job):
-    for key_set in group_dictionary:
-        d = key_set['department']
-        j = key_set['job']
-        if d == department and j == job:
-            return group_dictionary[key_set]
-    return None
+class XGroup(XImplementation):
+    def __init__(self, group_dictionary):
+        self._dict = group_dictionary
+
+    def __iter__(self):
+        for group_keys, records in self._dict.items():
+            print("iter", group_keys)
+            yield group_keys, XSet.null
+
+    def __hash__(self):
+        return hash(self._dict)
+
+    def __len__(self):
+        return len(self._dict)
+
+    def __repr__(self):
+        return 'XGroup()'
 
 
 class TestGroup:
@@ -63,10 +74,26 @@ hendrickson
         for person, scope in peeps:
             keys = person.re_scope(scopes)
             group_dictionary[keys].append((person, scope))
-        it_serfs = find(group_dictionary, 'it', 'serf')
+        it_serfs = self.find(group_dictionary, 'it', 'serf')
         assert len(it_serfs) == 2
         for peep, s in it_serfs:
             assert peep['job'] == 'serf'
+        # print()
+        # for key_set, records in group_dictionary.items():
+        #     d = key_set['department']
+        #     j = key_set['job']
+        #     print(f'dept {d} job {j}')
+        #     for e,s in records:
+        #         print(f"    {s}: {e['department']}, {e['job']}, {e['pay']}")
+        # assert False
+
+    def find(self, group_dictionary, department, job):
+        for key_set in group_dictionary:
+            d = key_set['department']
+            j = key_set['job']
+            if d == department and j == job:
+                return group_dictionary[key_set]
+        return None
 
     def build_peeps(self):
         e1 = SetBuilder() \
@@ -111,4 +138,26 @@ hendrickson
             .set()
         peeps = XSet.n_tuple((e1, e2, e3, e4, e5, e6, e7, e8))
         return peeps
+
+    def test_build_x_group(self):
+        peeps = self.build_peeps()
+        scopes = SetBuilder()\
+            .put("department", "department")\
+            .put("job", "job")\
+            .set()
+        group_dictionary = defaultdict(list)
+        for person, scope in peeps:
+            keys = person.re_scope(scopes)
+            group_dictionary[keys].append((person, scope))
+        x_group = XGroup(group_dictionary)
+        group_set = XSet(x_group)
+        xset_it = iter(group_set)
+        x1, s = next(xset_it)
+        assert x1['department'] == 'it'
+        assert x1['job'] == 'serf'
+        x2, s = next(xset_it)
+        assert x2['department'] == 'it'
+        assert x2['job'] == 'sdet'
+        x3, s = next(xset_it)
+        assert x3['department'] == 'sales' and x3['job'] == 'closer'
 
