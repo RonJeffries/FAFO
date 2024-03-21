29dff30eb965c1e74bc66b86077801103ec32162
Ron Jeffries <ronjeffries@acm.org>
2024-03-21 07:19:58 -0400

    StatsMaker can count.

--- tst/test_stats_object.py
+++ tst/test_stats_object.py
@@ -4,7 +4,15 @@ class StatsMaker:
         self._count = 0
         self._sum = 0
 
+    def value(self, number):
+        self._count += 1
+
 
 class TestStatsObject:
     def test_stats(self):
         stats = StatsMaker("pay")
+        stats.value(1000)
+        stats.value(2000)
+        stats.value(3000)
+        stats.value(4000)
+        assert stats._count == 4
